8d071a6d489232e96a704b616a07983b34389968
jossiwolf <0003jw@heilwig.de>
2016-06-20 15:00:48 +0200

    Added icon size change on select

--- js/bottom_tabs.js
+++ js/bottom_tabs.js
@@ -3,11 +3,19 @@ var style = $('<style>.ink.animate { animation: ' + 'ripple ' + eval(3 / 26000 *
 $('html > head').append(style);
 $(".bottom_tabs").css('background', $(".bottom_tabs > .tab > a.active").attr('data-ripple-color'));
 $(".bottom_tabs").attr('data-background-color', $(".bottom_tabs > .tab > a.active").attr('data-ripple-color'));
+$(".bottom_tabs > .tab > a.active").find('.material-icons').css({
+    'font-size': eval($(".bottom_tabs > .tab > a.active").find('.material-icons').css('font-size').substring(0, $(".bottom_tabs > .tab > a.active").find('.material-icons').css('font-size').indexOf('p')) * 1.07) + 'px'
+});
 
 $(".bottom_tabs > .tab > a").click(function(e) {
     $(".material-icons").each(function(index) {
-      $(this).css({'font-size': '24px'});
+        $(this).css({
+            'font-size': '24px'
+        });
     });
+    $(this).find('.material-icons').animate({
+        'font-size': eval($(this).find('.material-icons').css('font-size').substring(0, $(this).find('.material-icons').css('font-size').indexOf('p')) * 1.07) + 'px'
+    }, eval(eval(3 / 26000 * $('.bottom_tabs').width() + 557 / 1300) / 4));
     parent = $(this).parent().parent();
     if (parent.find(".ink").length == 0) {
         parent.prepend("<span class='ink'></span>");
@@ -29,9 +37,6 @@ $(".bottom_tabs > .tab > a").click(function(e) {
         background: $(this).attr('data-ripple-color')
     }).addClass("animate");
     parent.attr('data-background-color', $(this).attr('data-ripple-color'));
-    /*$(this).find('.material-icons').animate({
-        'font-size': eval($(this).find('.material-icons').css('font-size').substring(0, $(this).find('.material-icons').css('font-size').indexOf('p')) * 1.2) + 'px'
-    }, eval(3 / 26000 * $('.bottom_tabs').width() + 557 / 1300) * 2);*/
 });
 
 $("ul.bottom_tabs").bind('oanimationend animationend webkitAnimationEnd', function(e) {
